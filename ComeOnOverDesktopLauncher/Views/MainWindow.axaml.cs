eb83a346a5c182cf079cff2dc28c917071c2f90c
LewisIsWorking <38006444+LewisIsWorking@users.noreply.github.com>
2026-04-15 12:30:17 +0100

    initial commit: add Avalonia application structure and configuration files

---
+++ ComeOnOverDesktopLauncher/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ComeOnOverDesktopLauncher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
